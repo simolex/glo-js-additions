12ed0c120cec0e6403396fbcf7f934f65139d86a
simolex <simolex@yandex.ru>
2022-01-31 15:09:23 +0300

    Выполнено дополнительное задание 3

--- lesson-03/add-03.js
+++ lesson-03/add-03.js
@@ -1,1 +1,42 @@
 "use strict";
+
+let lang;
+const langAll = ["en", "ru"];
+
+do {
+  lang = prompt("Введите значение Lang = en | ru:");
+} while (!langAll.includes(lang));
+
+const weekEn = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+const weekRu = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+
+console.log("Задание 1 (через if)");
+if (lang == "en") {
+  console.log(weekEn);
+} else {
+  console.log(weekRu);
+}
+
+console.log("Задание 1 (switch-case)");
+switch (lang) {
+  case "en":
+    console.log(weekEn);
+    break;
+  case "ru":
+    console.log(weekRu);
+    break;
+}
+
+console.log("Задание 1 (через многомерный массив без ифов и switch)");
+let week = [];
+week["en"] = weekEn;
+week["ru"] = weekRu;
+console.log(week[lang]);
+
+//Задание 2
+
+const namePerson = prompt("Введите Имя: ");
+
+const rank =
+  namePerson == "Артем" ? "директор" : namePerson == "Александр" ? "преподаватель" : "студент";
+console.log(`Это: ${rank}`);
