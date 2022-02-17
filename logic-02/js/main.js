1cdd8a4be3947d360719056252ab4c21713eded3
simolex <simolex@yandex.ru>
2022-02-18 00:25:48 +0300

    Созданы классы для каждой сущности игрового поля

--- logic-02/js/main.js
+++ logic-02/js/main.js
@@ -1,5 +1,6 @@
 class Switch {
   constructor(switchId) {
+    this.switchId = switchId;
     this.isHot = false;
     this.isPowerOn = false;
     this.switchButton = document.getElementById(switchId);
@@ -19,4 +20,61 @@ const switchFirst = new Switch("switch-1");
 const switchSecond = new Switch("switch-2");
 const switchThree = new Switch("switch-3");
 
-console.log(switchFirst.switchButton);
+class SelectorBulb {
+  constructor(selectorId) {
+    this.lightBulb = 1;
+    this.selector = document.getElementById(selectorId);
+    this.selector.value = 1;
+    this.selector.addEventListener("input", (e) => {
+      this.setState(e.target.value);
+    });
+  }
+  setState(value) {
+    this.lightBulb = +value;
+  }
+}
+
+const selectorFirst = new SelectorBulb("range-1");
+
+console.log(switchFirst, selectorFirst);
+
+class LightBulb {
+  isOn;
+  constructor(lightId) {
+    this.lightId = lightId;
+    this.lightBulb = document.getElementById(lightId);
+  }
+  setState(isOn) {
+    this.isOn = isOn;
+  }
+  showState() {
+    const img = this.lightBulb.querySelector("img");
+    if (this.isOn) {
+      img.src = "img/lightbulb-on.svg";
+    } else {
+      img.src = "img/lightbulb-off.svg";
+    }
+  }
+}
+
+const lightFirst = new LightBulb("light-1");
+
+console.log(lightFirst);
+
+class Button {
+  constructor(buttonId) {
+    this.buttonId = buttonId;
+    this.element = document.getElementById(buttonId);
+    this.element.addEventListener("click", () => {
+      this.action();
+    });
+  }
+
+  action() {
+    console.log("Click: " + this.buttonId);
+  }
+}
+
+const buttonFirst = new Button("button-start");
+
+console.log(buttonFirst);
